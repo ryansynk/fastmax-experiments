478b520e566cb6c74ccdd72e9e58e6101b10f863
Monte Hoover <monte.b.hoover@gmail.com>
2024-04-03 15:06:40 -0400

    More run scripts.

--- finetune_QLoRA_fastmaxcpp.sh
+++ finetune_QLoRA_fastmaxcpp.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=LoraLinearMaxCppFlan                             # Specify a name for your job
-#SBATCH --output=slurm-logs/out-flan-linearmaxcpp-%j.log         # Specify the output log file
+#SBATCH --job-name=LoraFastmaxFlan                             # Specify a name for your job
+#SBATCH --output=slurm-logs/out-flan-fastmax-%j.log         # Specify the output log file
 #SBATCH --nodes=1                                             # Number of nodes to request
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=2                                    # Number of CPU cores per task
+#SBATCH --cpus-per-task=2                                     # Number of CPU cores per task
 #SBATCH --time=24:00:00                                       # Maximum execution time (HH:MM:SS)
+# #SBATCH --account=cml-ramani
+# #SBATCH --partition=cml-dpart
 #SBATCH --qos=high                                            # Specify the partition (queue) you want to use
 #SBATCH --gres=gpu:rtxa6000:1                                # Number of GPUs per node
 #SBATCH --mem=64G                                             # Memory per node
@@ -22,4 +24,4 @@ conda activate cmsc720
 
 # run finetune, saving to gpt_root so nexus backs up fine tuned models but leaving base models on scratch
 # python3 finetune/lora.py --io.train_data_dir $scratch_root/data/flan --io.val_data_dir $scratch_root/data/flan --io.checkpoint_dir $scratch_root/checkpoints/meta-llama/Llama-2-7b-hf --io.out_dir $scratch_root/out/lora_weights/Llama2-Flan-Quad-NoQuant --train.micro_batch_size 1 --precision bf16-true --devices 1
-python3 finetune/lora.py --attn_alg linearmax_cpp --io.train_data_dir $scratch_root/data/flan --io.val_data_dir $scratch_root/data/flan --io.checkpoint_dir $scratch_root/checkpoints/meta-llama/Llama-2-7b-hf --io.out_dir $scratch_root/out/lora_weights/Llama2-Flan-linearmaxcpp-NoQuant --train.micro_batch_size 1 --precision bf16-true --devices 1
+python3 finetune/lora.py --attn_alg fastmax_cpp --io.train_data_dir $scratch_root/data/flan --io.val_data_dir $scratch_root/data/flan --io.checkpoint_dir $scratch_root/checkpoints/meta-llama/Llama-2-7b-hf --io.out_dir $scratch_root/out/lora_weights/Llama2-Flan-fastmax-NoQuant --train.micro_batch_size 1 --precision bf16-true --devices 1
